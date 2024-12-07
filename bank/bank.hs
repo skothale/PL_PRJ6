0f0e82ff07a745c8d232d386ab994956a4803d70
Shivkumar Kothale <skothale@cs.binghamton.edu>
2024-12-07 00:10:20 -0500

    bank

--- bank/bank.hs
+++ bank/bank.hs
@@ -1,12 +1,17 @@
 module Bank where
 
-newtype BankOp a = ???
+newtype BankOp a = BankOp { runBankOp :: Float -> (a, Float) }
 
 deposit :: Float -> BankOp ()
+deposit amount = BankOp $ \balance -> ((), balance + amount)
 
 withdraw :: Float -> BankOp Float
+withdraw amount = BankOp $ \balance ->
+    if balance >= amount
+        then (amount, balance - amount)
+        else (-(amount - balance), 0)  -- Return negative overdraft amount
 
 runBankOp :: BankOp a -> a
-
+runBankOp op = evalStateT op initialState
 
 
